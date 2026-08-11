bc22fc6409e6edc8fdb38497cc850a98bfb52fa9
JoaoVictor-Lucena <victorlucena25@outlook.com>
2026-08-11 17:37:54 -0300

    Fix: Ajustando fuso horário do servidor para o Brasil

--- src/main/java/br/com/gerenciadortrader/GerenciadorTraderApplication.java
+++ src/main/java/br/com/gerenciadortrader/GerenciadorTraderApplication.java
@@ -1,10 +1,18 @@
 package br.com.gerenciadortrader;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
-public class GerenciadorTraderApplication {
+public class 	GerenciadorTraderApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorTraderApplication.class, args);
